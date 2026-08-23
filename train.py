c1a5dacfef37dcdd1deb9ee65b983feaae0d29ec
hagar3bdelsalam <ha2ar3bdelsalam@gmail.com>
2026-08-23 05:40:48 +0300

    fix: correct dataset reference in get_ds function for maximum length calculation

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ def get_ds(config):
     max_len_src = 0
     max_len_tgt = 0
 
-    for item in ds_raw:
+    for item in train_ds_raw:
         src_ids = tokenizer_src.encode(item[config['lang_src']]).ids
         tgt_ids = tokenizer_tgt.encode(item[config['lang_tgt']]).ids
         max_len_src = max(max_len_src, len(src_ids))
